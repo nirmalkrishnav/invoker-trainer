cec46e8906da8b0ff1769fe00afa6733010bbafe
meuequalsD <nirmalkrishnav@gmail.com>
2019-06-21 00:10:03 +0530

    WOW 1hour of pure js code phew
    WOW 1hour of pure js code phew

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# invoker-trainer
-Browser based Invoker spell trainer 
+## Glorious Invokations
+
+Welcome to browser based **Invoker** spell trainer. Now you can practice combos without running DotA2.
+
+*App is in development. Please write your suggestions as issues to this repo*
+
+want to contribute? its the usual setup - 
+1. Npm install
+2. npm start
+
---
+++ src/index.js
@@ -0,0 +1,91 @@
+
+
+
+
+// import invoker from './invoker.js'
+
+// need array or have to block non alpha keycodes
+let currentlyHolding = '';
+const audioPath = './assets/sounds/';
+// remap of hotkeys planned from legacy
+// lazy to add all :}
+const spells = {
+    qqq: {
+        keycode: 'qqq',
+        name: 'Cold Snap',
+        audioURL: 'qqq/Cold_Snap.mp3'
+    },
+    eee: {
+        keycode: 'eee',
+        name: 'Sunstrike',
+        audioURL: 'eee/Sun_Strike.mp3'
+    },
+    www: {
+        keycode: 'www',
+        name: 'EMP',
+        audioURL: 'www/EMP.mp3'
+    }
+};
+
+var invoked = {};
+
+var e0 = document.querySelector('h2');
+var el = document.querySelector('h1');
+
+document.addEventListener('keyup', event => {
+    if (event.defaultPrevented) {
+        return;
+    }
+    var key = event.key || event.keyCode;
+
+    if (key == 'R' || key == 'r') {
+        gloriousInvokation();
+    } else if (event.keyCode == 32) {
+        castInvoked();
+    } else {
+        addToHolding(key);
+    }
+
+});
+
+function addToHolding(key) {
+    if (currentlyHolding.length >= 3) {
+        clearText();
+    }
+    e0.innerHTML += key;
+    currentlyHolding = currentlyHolding + key;
+}
+
+function gloriousInvokation() {
+    let prop = spells[currentlyHolding];
+    if (!isNullOrUndefined(prop)) {
+        el.innerHTML = prop.name;
+        invoked = prop;
+        playAudio('invoke/Invoke.mp3');
+    } else {
+        clearText();
+    }
+}
+
+async function castInvoked() {
+    var result = await playAudio(invoked.audioURL);
+}
+
+function isNullOrUndefined(val) {
+    return (val === null || val === undefined ? true : false);
+}
+
+function clearText() {
+    el.innerHTML = '';
+    currentlyHolding = '';
+    e0.innerHTML = '';
+    invoked = {};
+}
+
+function playAudio(fileName) {
+    return new Promise(resolve => {
+        var audio = new Audio(audioPath + fileName);
+        audio.play();
+        resolve();
+    });
+}
